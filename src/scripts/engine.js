89493f1ef772fec3077c393b69b35e387f94c31b
ReisThaiss <reis.thais@hotmail.com>
2024-11-26 16:19:39 -0300

    project created

---
+++ src/scripts/engine.js
@@ -0,0 +1,46 @@
+const pianoKeys = document.querySelectorAll(".piano-Keys .key");
+
+const volumeSlider = document.querySelector(".volume-slider input");
+
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedKeys = [];
+
+let audio = new Audio("src/tunes");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+}; 
+
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+document.addEventListener("keydown", (e)=> {
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+    console.log(e.key);
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+    console.log(e.target.value);
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+keysCheck.addEventListener("click",showHideKeys);
